359a8010fb3abf7e5e5648d6c19d5cd74b0ee4ef
v645 <45901718+v645@users.noreply.github.com>
2021-12-06 11:21:28 +0300

    agents fix

--- problem-solver/py/services/HelloAgent/HarvestAgent.py
+++ problem-solver/py/services/HelloAgent/HarvestAgent.py
@@ -7,10 +7,12 @@ class HarvestAgent(ScAgent):
 
    def RunImpl(self, evt: ScEventParams) -> ScResult:
        # finding start point
+       print("finding start point")
        ctx = ScMemoryContext.Create("context")
        start = ctx.HelperResolveSystemIdtf("HarvestAgent", ScType.Node)
 
        # finding place's area
+       print("finding place's area")
        nrel_area = ctx.HelperResolveSystemIdtf("nrel_area", ScType.NodeConstNoRole)
        placeNodeIter = ctx.Iterator3(start, ScType.EdgeAccessConstPosPerm, ScType.Node)
        while placeNodeIter.Next():
@@ -20,29 +22,38 @@ class HarvestAgent(ScAgent):
            while areaIter.Next():
                placeAreaNode = areaIter.Get(2)
 
-        # find result productivity construction
-       nrel_result_productivity = ctx.HelperResolveSystemIdtf("nrel_result_productivity", ScType.NodeConstRole)
-       resultProductivityIter = ctx.Iterator5(ScType.NodeConstTuple, ScType.EdgeDCommonConst, ScType.Node,
-                                              ScType.EdgeAccessConstPosPerm,
-                                              nrel_result_productivity)
-       while resultProductivityIter.Next():
-           resultProductivityNode = resultProductivityIter.Get(2)
+               # finding result collection
+               print("find result collection")
+               rrel_field = ctx.HelperResolveSystemIdtf("rrel_field", ScType.NodeConstRole)
+               resultCollectionIter = ctx.Iterator5(ScType.NodeConstTuple, ScType.EdgeAccessConstPosPerm, placeNode,
+                                                      ScType.EdgeAccessConstPosPerm,
+                                                      rrel_field)
+               while resultCollectionIter.Next():
+                    resultCollectNode = resultCollectionIter.Get(0)
+                    # Find result productivity construction
+                    print("find result productivity construction")
+                    nrel_result_productivity = ctx.HelperResolveSystemIdtf("nrel_result_productivity", ScType.NodeConstRole)
+                    resultProductivityIter = ctx.Iterator5(resultCollectNode, ScType.EdgeDCommonConst, ScType.Node,
+                                                      ScType.EdgeAccessConstPosPerm,
+                                                      nrel_result_productivity)
+                    while resultProductivityIter.Next():
+                        resultProductivityNode = resultProductivityIter.Get(2)
 
-           # calculating harvest value
-           areaValue = ctx.HelperGetSystemIdtf(placeAreaNode)
-           area = int(areaValue)
-           resultProductivityValue = ctx.HelperGetSystemIdtf(resultProductivityNode)
-           resultProductivity = int(resultProductivityValue)
-           harvest = area * resultProductivity
+                        # calculating harvest value
+                        print("calculating harvest value")
+                        areaValue = ctx.HelperGetSystemIdtf(placeAreaNode)
+                        area = int(areaValue)
+                        resultProductivityValue = ctx.HelperGetSystemIdtf(resultProductivityNode)
+                        resultProductivity = float(resultProductivityValue)
+                        harvest = area * resultProductivity
 
-           # find result collection
-           resultCollectNode = resultProductivityIter.Get(0)
-           # creating new result construction
-           resultHarvestNode = ctx.CreateNode(ScType.Node)
-           ctx.HelperSetSystemIdtf(str(harvest), resultHarvestNode)
-           nrel_result_harvest = ctx.HelperResolveSystemIdtf("nrel_result_harvest", ScType.NodeConstRole)
-           edgeToResult = ctx.CreateEdge(ScType.EdgeDCommonConst, resultCollectNode, resultHarvestNode)
-           edgeResultRelation = ctx.CreateEdge(ScType.EdgeAccessConstPosPerm, nrel_result_harvest, edgeToResult)
+                        # creating new result construction
+                        print("creating new result construction")
+                        resultHarvestNode = ctx.CreateNode(ScType.Node)
+                        ctx.HelperSetSystemIdtf(str(harvest), resultHarvestNode)
+                        nrel_result_harvest = ctx.HelperResolveSystemIdtf("nrel_result_harvest", ScType.NodeConstRole)
+                        edgeToResult = ctx.CreateEdge(ScType.EdgeDCommonConst, resultCollectNode, resultHarvestNode)
+                        edgeResultRelation = ctx.CreateEdge(ScType.EdgeAccessConstPosPerm, nrel_result_harvest, edgeToResult)
 
        print("Agent successeded")
        return ScResult.Ok
--- problem-solver/py/services/HelloAgent/ProfitAgent.py
+++ problem-solver/py/services/HelloAgent/ProfitAgent.py
@@ -6,46 +6,64 @@ class ProfitAgent(ScAgent):
 
     def RunImpl(self, evt: ScEventParams) -> ScResult:
         # finding start point
+        print("finding start point")
         ctx = ScMemoryContext.Create("context")
         start = ctx.HelperResolveSystemIdtf("ProfitAgent", ScType.Node)
 
-        # find result harvest construction
-        nrel_result_harvest = ctx.HelperResolveSystemIdtf("nrel_result_harvest", ScType.NodeConstRole)
-        resultHarvestIter = ctx.Iterator5(ScType.NodeConstTuple, ScType.EdgeDCommonConst, ScType.Node,
+        # finding place node
+        print("finding place node")
+        placeNodeIter = ctx.Iterator3(start, ScType.EdgeAccessConstPosPerm, ScType.Node)
+        while placeNodeIter.Next():
+            placeNode = placeNodeIter.Get(2)
+            # finding result collection
+            print("find result collection")
+            rrel_field = ctx.HelperResolveSystemIdtf("rrel_field", ScType.NodeConstRole)
+            resultCollectionIter = ctx.Iterator5(ScType.NodeConstTuple, ScType.EdgeAccessConstPosPerm, placeNode,
+                                             ScType.EdgeAccessConstPosPerm,
+                                             rrel_field)
+            while resultCollectionIter.Next():
+                resultCollectNode = resultCollectionIter.Get(0)
+                # find result harvest construction
+                print("find result harvest construction")
+                nrel_result_harvest = ctx.HelperResolveSystemIdtf("nrel_result_harvest", ScType.NodeConstRole)
+                resultHarvestIter = ctx.Iterator5(resultCollectNode, ScType.EdgeDCommonConst, ScType.Node,
                                           ScType.EdgeAccessConstPosPerm,
                                           nrel_result_harvest)
-        while resultHarvestIter.Next():
-            resultHarvestNode = resultHarvestIter.Get(2)
-            # find culture attached to this construction
-            rrel_culture = ctx.HelperResolveSystemIdtf("rrel_culture", ScType.NodeConstRole)
-            resultCollectionNode = resultHarvestIter.Get(0)
-            resultCultureIter = ctx.Iterator5(resultCollectionNode, ScType.EdgeAccessConstPosPerm, ScType.Node,
+                while resultHarvestIter.Next():
+                    resultHarvestNode = resultHarvestIter.Get(2)
+                    # find culture attached to this construction
+                    print("find culture attached to this construction")
+                    rrel_culture = ctx.HelperResolveSystemIdtf("rrel_culture", ScType.NodeConstRole)
+                    resultCultureIter = ctx.Iterator5(resultCollectNode, ScType.EdgeAccessConstPosPerm, ScType.Node,
                                               ScType.EdgeAccessConstPosPerm,
                                               rrel_culture)
-            while resultCultureIter.Next():
-                cultureNode = resultCultureIter.Get(2)
-                # fing culture's cost per ton
-                nrel_cost_per_ton = ctx.HelperResolveSystemIdtf("nrel_cost_per_ton", ScType.NodeConstNoRole)
-                costPerTonIter = ctx.Iterator5(cultureNode, ScType.EdgeDCommonConst, ScType.Node,
+                    while resultCultureIter.Next():
+                        cultureNode = resultCultureIter.Get(2)
+                        # fing culture's cost per ton
+                        print("fing culture's cost per ton")
+                        nrel_cost_per_ton = ctx.HelperResolveSystemIdtf("nrel_cost_per_ton", ScType.NodeConstNoRole)
+                        costPerTonIter = ctx.Iterator5(cultureNode, ScType.EdgeDCommonConst, ScType.Node,
                                                ScType.EdgeAccessConstPosPerm,
                                                nrel_cost_per_ton)
-                while costPerTonIter.Next():
-                    costPerTonNode = costPerTonIter.Get(2)
+                        while costPerTonIter.Next():
+                            costPerTonNode = costPerTonIter.Get(2)
 
-            # calculating productivity value
-            harvestValue = ctx.HelperGetSystemIdtf(resultHarvestNode)
-            harvest = int(harvestValue)
-            costPerTonValue = ctx.HelperGetSystemIdtf(costPerTonNode)
-            costPerTon = int(costPerTonValue)
-            resultProfit = (harvest / 1000) * costPerTon
+                            # calculating productivity value
+                            print("calculating productivity value")
+                            harvestValue = ctx.HelperGetSystemIdtf(resultHarvestNode)
+                            harvest = float(harvestValue)
+                            costPerTonValue = ctx.HelperGetSystemIdtf(costPerTonNode)
+                            costPerTon = int(costPerTonValue)
+                            resultProfit = (harvest / 1000) * costPerTon
 
-            # creating result construction
-            resultProfitNode = ctx.CreateNode(ScType.Node)
-            ctx.HelperSetSystemIdtf(str(resultProfit), resultProfitNode)
-            nrel_result_profit = ctx.HelperResolveSystemIdtf("nrel_result_profit",
+                            # creating result construction
+                            print("creating result construction")
+                            resultProfitNode = ctx.CreateNode(ScType.Node)
+                            ctx.HelperSetSystemIdtf(str(resultProfit), resultProfitNode)
+                            nrel_result_profit = ctx.HelperResolveSystemIdtf("nrel_result_profit",
                                                              ScType.NodeConstRole)
-            edgeToResult = ctx.CreateEdge(ScType.EdgeDCommonConst, resultCollectionNode, resultProfitNode)
-            edgeResultRelation = ctx.CreateEdge(ScType.EdgeAccessConstPosPerm, nrel_result_profit,
+                            edgeToResult = ctx.CreateEdge(ScType.EdgeDCommonConst, resultCollectNode, resultProfitNode)
+                            edgeResultRelation = ctx.CreateEdge(ScType.EdgeAccessConstPosPerm, nrel_result_profit,
                                                 edgeToResult)
 
         print("Agent successeded")
